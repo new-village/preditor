c5002e2e00f5bb796c9bae7d356238893d2e462a
New-Village <kazuki.niimura@gmail.com>
2018-03-30 09:10:20 +0900

    add service up shell

---
+++ startup.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+EXC_USER=centos
+VENV_DIR=/home/${EXC_USER}/.venv/preditor/
+SHL_NAME=`echo ${0##*/}|sed -e "s/.sh//g"`
+SHL_DIR=$(cd $(dirname $0); pwd)
+SHL_FILE=${SHL_DIR}/${SHL_NAME}.sh
+
+cd ${SHL_DIR}
+source ${VENV_DIR}/bin/activate
+sudo `which python` ${SHL_DIR}/manage.py runserver 0.0.0.0:80 &
+sudo -u ${EXC_USER} `which python` ${SHL_DIR}/manage.py shell_plus --notebook &
+sudo -u ${EXC_USER} `which python` ${SHL_DIR}/manage.py collector
+
+exit
