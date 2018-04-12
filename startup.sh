7fb80f67997bba09c3d6b9bb0964e0f35eeb2104
New-Village <kazuki.niimura@gmail.com>
2018-04-12 17:25:07 +0900

    bugfix: startup.sh

--- startup.sh
+++ startup.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 EXC_USER=centos
-VENV_DIR=/home/${EXC_USER}/.venv/preditor/
+VENV_DIR=/home/${EXC_USER}/.venv/preditor
 SHL_NAME=`echo ${0##*/}|sed -e "s/.sh//g"`
 SHL_DIR=$(cd $(dirname $0); pwd)
 SHL_FILE=${SHL_DIR}/${SHL_NAME}.sh
@@ -10,6 +10,6 @@ cd ${SHL_DIR}
 source ${VENV_DIR}/bin/activate
 sudo `which python` ${SHL_DIR}/manage.py runserver --insecure 0.0.0.0:80 &
 sudo -u ${EXC_USER} `which python` ${SHL_DIR}/manage.py shell_plus --notebook &
-sudo -u ${EXC_USER} `which python` ${SHL_DIR}/manage.py collector &
+sudo -u ${EXC_USER} `which python` ${SHL_DIR}/manage.py collector
 
 exit
