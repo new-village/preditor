94b3d5dfdc407e16ac2cf85e515cde26a024c2ef
New-Village <kazuki.niimura@gmail.com>
2018-05-05 01:05:30 +0900

    Bugfix: delete resutl_flg is False from delete query filter

--- umap/management/commands/collector.py
+++ umap/management/commands/collector.py
@@ -54,7 +54,7 @@ class Command(BaseCommand):
         # Delete uncompleted data
         print(str_now() + " [DELETE]")
         Race.objects.filter(race_dt__lt=latest, result_flg=False).delete()
-        Result.objects.filter(result_flg=False, horse_num__isnull=True).delete()
+        Result.objects.filter(horse_num__isnull=True).delete()
 
         # Calc explanatory variable
         enrich_data(Race.objects.filter(race_dt__gte=fore_end(from_dt)))
